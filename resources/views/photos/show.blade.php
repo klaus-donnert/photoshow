b7e3a0dae4d8b4a355eb0fea6d9150cc09f6d2b5
Klaus Donnert <dieter.donnert@gmail.com>
2017-12-07 05:52:03 -0600

    initial commit

---
+++ resources/views/photos/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <h3>{{$photo->title}}</h3>
+    <p>{{$photo->description}}</p>
+    <a href="/albums/{{$photo->album_id}}">Back</a>
+    <hr>
+    <img src="/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+    <br>
+    <br>
+    {!! Form::open(['action' => ['PhotosController@destroy', $photo->id], 'method' =>'post']) !!}
+    {{Form::hidden('_method','DELETE')}}
+    {{Form::submit('Delete',['class' => 'button alert'])}}
+    {!! Form::close() !!}
+    <hr>
+    <small>Size: {{$photo->size}}</small>
+@endsection
