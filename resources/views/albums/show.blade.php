a4c33acc1e8fe7584959e5f20861cfb947542a6f
klaus-donnert <dieter.donnert@gmail.com>
2017-12-08 13:51:04 -0600

    added an else clause

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -20,7 +20,7 @@
 
                             </a>
                             <br>
-                            <img src="{!! storage_path('photos/'.$photo->album_id.'/'.$photo->photo) !!}">
+                            <img src="{!! URL::to('photos/'.$photo->album_id.'/'.$photo->photo) !!}" alt="test image">
                             <br>
                             <h4>{{$photo->title}}</h4>
                             @else
